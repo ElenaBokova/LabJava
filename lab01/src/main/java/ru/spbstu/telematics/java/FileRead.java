c2a2aef6b6e5e9858a4753d3fa9ec62084c43864
ElenaBokova <58823723+ElenaBokova@users.noreply.github.com>
2020-01-22 13:29:02 +0300

    Add files via upload

---
+++ lab01/src/main/java/ru/spbstu/telematics/java/FileRead.java
@@ -0,0 +1,25 @@
+package ru.spbstu.telematics.java;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileRead {
+	
+    public static String readS() throws IOException
+    {   String tmp = "";
+        FileReader reader = new FileReader("C:\\file.txt");
+        int c;
+        try {
+                 while ((c = reader.read()) != -1)
+                {
+                tmp += (char) c;
+                }
+            } catch(IOException ex)
+        {
+        System.out.println(ex.getMessage());
+         }
+ return tmp;
+    }
+
+
+}
